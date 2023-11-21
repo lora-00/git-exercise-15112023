7a1bf8170a3609dc6c4b9407bc394e091f65e343
lora <lora.ilkova00@gmail.com>
2023-11-21 11:21:14 +0200

    division, abs sustraction added; introduced authorization

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -4,10 +4,26 @@ namespace GitExercise
 {
     public class Startup
     {
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+        private const string Password = "abcd1234";
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+            if(!isAuthorized)
+            {
+                Console.WriteLine("Access denied!");
+                Console.ReadKey(intercept:true);
+                return;
+            }
             Console.WriteLine("Console Calculator App");
-            Console.WriteLine(new string('-', 15));
+            Console.WriteLine(new string(c:'-', count:15));
 
             Console.Write("a = ");
             double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
@@ -34,6 +50,78 @@ namespace GitExercise
                     break;
                 case "m":
                     OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubstractAbs(a, b);
+                    break;
+            }
+
+            Console.WriteLine("Pres any key to close the app...");
+            Console.ReadKey(true);
+        }
+    }
+}
+using System;
+
+namespace GitExercise
+{
+    public class Startup
+    {
+        private static bool CheckCredentials()
+        {
+            Console.Write("Enter password to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+        private const string Password = "abcd1234";
+        public static void Main()
+        {
+            bool isAuthorized = CheckCredentials();
+            if(!isAuthorized)
+            {
+                Console.WriteLine("Access denied!");
+                Console.ReadKey(intercept:true);
+                return;
+            }
+            Console.WriteLine("Console Calculator App");
+            Console.WriteLine(new string(c:'-', count:15));
+
+            Console.Write("a = ");
+            double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.Write("b = ");
+            double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+            Console.WriteLine("Choose one from the listed options:");
+            foreach (string option in OptionsManager.OptionsList)
+            {
+                Console.WriteLine($"\t{option}");
+            }
+
+            Console.Write("Option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "a":
+                    OptionsManager.Add(a, b);
+                    break;
+                case "s":
+                    OptionsManager.Subtract(a, b);
+                    break;
+                case "m":
+                    OptionsManager.Multiply(a, b);
+                    break;
+                case "d":
+                    OptionsManager.Divide(a, b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubstractAbs(a, b);
                     break;
             }
 
